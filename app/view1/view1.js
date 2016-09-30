adb6c7fe4a99632ee0dd2a235fae6326bc574824
calvinvette <calvin.vette@gmail.com>
2016-09-30 09:01:48 +0000

    Added Customer Register & List functionality

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,17 +9,49 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', function($scope) {
+.controller('View1Ctrl', function($scope, $http) {
   $scope.firstName = "World";
   $scope.lastName = "";
   $scope.phoneNumber = "";
   $scope.email = "";
   $scope.customerId = 0;
 
-  $scope.register = function() {
-    console.log("%s %s registered!",
-        $scope.firstName,
-        $scope.lastName
+  $scope.customers = [];
+
+  $scope.retrieveCustomers = function() {
+    $http.get("http://localhost:1701/api/customers").then(
+        function(response) {
+          $scope.customers = response.data;
+          console.log("Customers found:");
+          console.log($scope.customers);
+        },function(response) {
+          console.log("Error retrieving customers: " + response.statusText);
+        }
     );
+  };
+
+  $scope.register = function() {
+    var customer = {
+      firstName : $scope.firstName,
+      lastName : $scope.lastName,
+      phoneNumber : $scope.phoneNumber,
+      email : $scope.email
+    };
+    $http.post("http://localhost:1701/api/customers", customer).then(
+        function(response) { // Success Callback
+          console.log("Success");
+          var customer = response.data;
+          console.log(customer);
+          console.log("%s %s registered!",
+              customer.firstName,
+              customer.lastName
+          );
+        },
+        function(response) { // Error Callback
+          console.log("Error posting customer: " + response.statusText);
+        }
+    );
+
+
   }
 });
